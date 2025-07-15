3c23c0275ecbd23955cd845c5d5efbb5b3e43231
Jikuhara <kei2003730@gmail.com>
2025-07-15 15:11:50 +0900

    2-2 終了

---
+++ Chapter2/Section2/firstThread2.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   firstThread2.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 14:50:08 by kei2003730        #+#    #+#             */
+/*   Updated: 2025/07/15 14:56:16 by kei2003730       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void *anotherFunc(int n)
+{
+	if (n == 1)
+	{
+		printf("Hasta la vista, baby.\n");
+		// pthread_exit(NULL);
+		exit(0);
+	}
+}
+
+void *threadFunc(void *arg)
+{
+	int i;
+
+	for (i = 0; i < 3; i++)
+	{
+		printf("I'm threadFunc %d\n", i);
+		anotherFunc(i);
+		sleep(1);
+	}
+	return (NULL);
+}
+
+int main(void)
+{
+	pthread_t	threads;
+	int 		i;
+
+	if (pthread_create(&threads, NULL, threadFunc, NULL) != 0)
+		{
+			perror("ERROR: Failed to create thread.\n");
+			exit(1);
+		}
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("I'm main: %d\n", i);
+		sleep(1);
+	}
+	return (0);
+}
